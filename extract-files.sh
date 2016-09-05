LineageOS/android_device_nubia_nx531j

nx531j: use cm extractutils to extract proprietary files instead
Change-Id: I8c4dcc90a9f8b349289bc41d266462b4f8a5aea3

extract-files.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-export DEVICE=nx531j
-export VENDOR=zte
+DEVICE=nx531j
+VENDOR=zte
 
-function pull() {
-  echo "Extracting /system/$1 ..."
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-  DIR=$(dirname $1)
-  if [ ! -d $2/$DIR ]; then
-    mkdir -p $2/$DIR
-  fi
+CM_ROOT="$MY_DIR"/../../..
 
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$1 $2/$1
-  else
-    cp $SRC/system/$1 $2/$1
-  fi
-}
-
-function extract() {
-  NOT_COMMENT_OR_BLANK='(^#|^$)'
-  MODULE_FILE='^[-!~].+'
-  LIBRARY_VARIENT='.+\.so\:.+\.so'
-
-  for FILE in $(egrep -v $NOT_COMMENT_OR_BLANK $1); do
-    if [[ $FILE =~ $MODULE_FILE ]]; then
-      pull ${FILE:1} $2
-    elif [[ $FILE =~ $LIBRARY_VARIENT ]]; then
-      pull $(echo $FILE | cut -d ':' -f 1) $2
-      pull $(echo $FILE | cut -d ':' -f 2) $2
-    else
-      pull $FILE $2
-    fi
-  done
-}
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
 if [ $# -eq 0 ]; then
   SRC=adb
@@ -53,9 +49,9 @@ else
   fi
 fi
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
-./setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-
-DEVICE=nx531j
-VENDOR=zte
-
-
-PROPRIETARY_FILES=proprietary-files.txt
-
-VENDOR_DIR=vendor/$VENDOR/$DEVICE
-
-MK_ANDROID=../../../$VENDOR_DIR/Android.mk
-
-MK_VENDOR=../../../$VENDOR_DIR/$DEVICE-vendor.mk
-MK_VENDOR_BLOBS=../../../$VENDOR_DIR/$DEVICE-vendor-blobs.mk
-
-MK_BOARD_CONFIG_VENDOR=../../../$VENDOR_DIR/BoardConfigVendor.mk
-
-HEADER="$(cat << EOF
+#
 # Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,218 +13,40 @@ HEADER="$(cat << EOF
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-EOF)"
+set -e
 
-NOT_COMMENT_OR_BLANK='(^#|^$)'
-BOOT_JAR='^~.+\.jar$'
-SIGNING_NEEDED='^[-!].+'
-LIBRARY_VARIENT='.+\.so\:.+\.so'
+DEVICE=nx531j
+VENDOR=zte
 
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-echo "$HEADER" > $MK_VENDOR_BLOBS
-echo "" >> $MK_VENDOR_BLOBS
-echo "PRODUCT_COPY_FILES += \\" >> $MK_VENDOR_BLOBS
+CM_ROOT="$MY_DIR"/../../..
 
-COUNT=$(wc -l $PROPRIETARY_FILES | awk {'print $1'})
-COUNT=$(expr $COUNT - $(egrep -c $NOT_COMMENT_OR_BLANK $PROPRIETARY_FILES))
-for FILE in $(egrep -v $NOT_COMMENT_OR_BLANK $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-  if [[ ! "$FILE" =~ $BOOT_JAR ]] \
-  && [[ ! "$FILE" =~ $SIGNING_NEEDED ]] \
-  && [[ ! "$FILE" =~ $LIBRARY_VARIENT ]]; then
-    if [ $COUNT != "0" ]; then
-      echo "    $VENDOR_DIR/proprietary/$FILE:system/$FILE \\" >> $MK_VENDOR_BLOBS
-    else
-      echo "    $VENDOR_DIR/proprietary/$FILE:system/$FILE" >> $MK_VENDOR_BLOBS
-    fi
-  fi
-done
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-echo "$HEADER" > $MK_VENDOR
+# Copyright headers and guards
+write_headers
 
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
-COUNT=$(egrep -c $SIGNING_NEEDED $PROPRIETARY_FILES)
-for LINE in $(egrep $SIGNING_NEEDED $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $BOOT_JAR $PROPRIETARY_FILES)
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_BOOT_JARS += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $BOOT_JAR $PROPRIETARY_FILES)
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $LIBRARY_VARIENT $PROPRIETARY_FILES)
-for LINE in $(egrep $LIBRARY_VARIENT $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE_32=$(echo "$LINE" | cut -d ':' -f 1)
-  FILE_64=$(echo "$LINE" | cut -d ':' -f 2)
-
-  MODULE=$(basename $FILE_32)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "\$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)" >> $MK_VENDOR
-
-
-echo "$HEADER" > $MK_BOARD_CONFIG_VENDOR
-
-
-echo "$HEADER" > $MK_ANDROID
-(cat << EOF) >> $MK_ANDROID
-
-LOCAL_PATH := \$(call my-dir)
-
-ifeq (\$(TARGET_DEVICE),$DEVICE)
-EOF
-
-echo "" >> $MK_ANDROID
-
-for LINE in $(egrep $SIGNING_NEEDED $PROPRIETARY_FILES); do
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  SUFFIX=${FILE##*.}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES := proprietary/$FILE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := APPS" >> $MK_ANDROID
-
-  if [[ "$FILE" =~ ^priv-app\/.* ]]; then
-    echo "LOCAL_PRIVILEGED_MODULE := true" >> $MK_ANDROID
-  elif [[ "$FILE" =~ ^vendor\/app\/.* ]]; then
-    echo "LOCAL_PROPRIETARY_MODULE := true" >> $MK_ANDROID
-  fi
-
-  case ${LINE:0:1} in
-    '!')
-      echo "LOCAL_CERTIFICATE := platform" >> $MK_ANDROID
-    ;;
-
-    '-')
-      echo "LOCAL_CERTIFICATE := shared" >> $MK_ANDROID
-    ;;
-  esac
-
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES := proprietary/$FILE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := JAVA_LIBRARIES" >> $MK_ANDROID
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-for LINE in $(egrep $LIBRARY_VARIENT $PROPRIETARY_FILES); do
-  FILE_32=$(echo "$LINE" | cut -d ':' -f 1)
-  FILE_64=$(echo "$LINE" | cut -d ':' -f 2)
-
-  MODULE=$(basename $FILE_32)
-  MODULE=${MODULE%.*}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MULTILIB := both" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES_64 := proprietary/$FILE_64" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES_32 := proprietary/$FILE_32" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := .so" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := SHARED_LIBRARIES" >> $MK_ANDROID
-
-  if [[ "$FILE_32" =~ ^vendor\/.* ]]; then
-    echo "LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_PROPRIETARY_MODULE := true" >> $MK_ANDROID
-  else
-    echo "LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_SHARED_LIBRARIES)" >> $MK_ANDROID
-  fi
-
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-(cat << EOF) >> $MK_ANDROID
+cat << EOF >> "$ANDROIDMK"
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
-endif
 EOF
+
+# We are done!
+write_footers

Demelza/device_nubia_nx531j

extract-files.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-export DEVICE=nx531j
-export VENDOR=nubia
+DEVICE=nx531j
+VENDOR=nubia
 
-function pull() {
-  echo "Extracting /system/$1 ..."
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-  DIR=$(dirname $1)
-  if [ ! -d $2/$DIR ]; then
-    mkdir -p $2/$DIR
-  fi
+MK_ROOT="$MY_DIR"/../../..
 
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$1 $2/$1
-  else
-    cp $SRC/system/$1 $2/$1
-  fi
-}
-
-function extract() {
-  NOT_COMMENT_OR_BLANK='(^#|^$)'
-  MODULE_FILE='^[-!~].+'
-  LIBRARY_VARIENT='.+\.so\:.+\.so'
-
-  for FILE in $(egrep -v $NOT_COMMENT_OR_BLANK $1); do
-    if [[ $FILE =~ $MODULE_FILE ]]; then
-      pull ${FILE:1} $2
-    elif [[ $FILE =~ $LIBRARY_VARIENT ]]; then
-      pull $(echo $FILE | cut -d ':' -f 1) $2
-      pull $(echo $FILE | cut -d ':' -f 2) $2
-    else
-      pull $FILE $2
-    fi
-  done
-}
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
 if [ $# -eq 0 ]; then
   SRC=adb
@@ -53,9 +49,9 @@ else
   fi
 fi
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
 
-extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
-./setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-
-DEVICE=nx531j
-VENDOR=nubia
-
-
-PROPRIETARY_FILES=proprietary-files.txt
-
-VENDOR_DIR=vendor/$VENDOR/$DEVICE
-
-MK_ANDROID=../../../$VENDOR_DIR/Android.mk
-
-MK_VENDOR=../../../$VENDOR_DIR/$DEVICE-vendor.mk
-MK_VENDOR_BLOBS=../../../$VENDOR_DIR/$DEVICE-vendor-blobs.mk
-
-MK_BOARD_CONFIG_VENDOR=../../../$VENDOR_DIR/BoardConfigVendor.mk
-
-HEADER="$(cat << EOF
+#
 # Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,218 +13,40 @@ HEADER="$(cat << EOF
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# This file is generated by device/$VENDOR/$DEVICE/setup-makefiles.sh
-EOF)"
+set -e
 
-NOT_COMMENT_OR_BLANK='(^#|^$)'
-BOOT_JAR='^~.+\.jar$'
-SIGNING_NEEDED='^[-!].+'
-LIBRARY_VARIENT='.+\.so\:.+\.so'
+DEVICE=nx531j
+VENDOR=nubia
 
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-echo "$HEADER" > $MK_VENDOR_BLOBS
-echo "" >> $MK_VENDOR_BLOBS
-echo "PRODUCT_COPY_FILES += \\" >> $MK_VENDOR_BLOBS
+MK_ROOT="$MY_DIR"/../../..
 
-COUNT=$(wc -l $PROPRIETARY_FILES | awk {'print $1'})
-COUNT=$(expr $COUNT - $(egrep -c $NOT_COMMENT_OR_BLANK $PROPRIETARY_FILES))
-for FILE in $(egrep -v $NOT_COMMENT_OR_BLANK $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-  if [[ ! "$FILE" =~ $BOOT_JAR ]] \
-  && [[ ! "$FILE" =~ $SIGNING_NEEDED ]] \
-  && [[ ! "$FILE" =~ $LIBRARY_VARIENT ]]; then
-    if [ $COUNT != "0" ]; then
-      echo "    $VENDOR_DIR/proprietary/$FILE:system/$FILE \\" >> $MK_VENDOR_BLOBS
-    else
-      echo "    $VENDOR_DIR/proprietary/$FILE:system/$FILE" >> $MK_VENDOR_BLOBS
-    fi
-  fi
-done
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
 
-echo "$HEADER" > $MK_VENDOR
+# Copyright headers and guards
+write_headers
 
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
-COUNT=$(egrep -c $SIGNING_NEEDED $PROPRIETARY_FILES)
-for LINE in $(egrep $SIGNING_NEEDED $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $BOOT_JAR $PROPRIETARY_FILES)
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_BOOT_JARS += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $BOOT_JAR $PROPRIETARY_FILES)
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "PRODUCT_PACKAGES += \\" >> $MK_VENDOR
-
-COUNT=$(egrep -c $LIBRARY_VARIENT $PROPRIETARY_FILES)
-for LINE in $(egrep $LIBRARY_VARIENT $PROPRIETARY_FILES); do
-  COUNT=$(expr $COUNT - 1)
-
-  FILE_32=$(echo "$LINE" | cut -d ':' -f 1)
-  FILE_64=$(echo "$LINE" | cut -d ':' -f 2)
-
-  MODULE=$(basename $FILE_32)
-  MODULE=${MODULE%.*}
-
-  if [ $COUNT != "0" ]; then
-    echo "    $MODULE \\" >> $MK_VENDOR
-  else
-    echo "    $MODULE" >> $MK_VENDOR
-  fi
-done
-
-echo "" >> $MK_VENDOR
-echo "\$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)" >> $MK_VENDOR
-
-
-echo "$HEADER" > $MK_BOARD_CONFIG_VENDOR
-
-
-echo "$HEADER" > $MK_ANDROID
-(cat << EOF) >> $MK_ANDROID
-
-LOCAL_PATH := \$(call my-dir)
-
-ifeq (\$(TARGET_DEVICE),$DEVICE)
-EOF
-
-echo "" >> $MK_ANDROID
-
-for LINE in $(egrep $SIGNING_NEEDED $PROPRIETARY_FILES); do
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  SUFFIX=${FILE##*.}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES := proprietary/$FILE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := APPS" >> $MK_ANDROID
-
-  if [[ "$FILE" =~ ^priv-app\/.* ]]; then
-    echo "LOCAL_PRIVILEGED_MODULE := true" >> $MK_ANDROID
-  elif [[ "$FILE" =~ ^vendor\/app\/.* ]]; then
-    echo "LOCAL_PROPRIETARY_MODULE := true" >> $MK_ANDROID
-  fi
-
-  case ${LINE:0:1} in
-    '!')
-      echo "LOCAL_CERTIFICATE := platform" >> $MK_ANDROID
-    ;;
-
-    '-')
-      echo "LOCAL_CERTIFICATE := shared" >> $MK_ANDROID
-    ;;
-  esac
-
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-for LINE in $(egrep $BOOT_JAR $PROPRIETARY_FILES); do
-  FILE=${LINE:1}
-
-  MODULE=$(basename $FILE)
-  MODULE=${MODULE%.*}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES := proprietary/$FILE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := JAVA_LIBRARIES" >> $MK_ANDROID
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-for LINE in $(egrep $LIBRARY_VARIENT $PROPRIETARY_FILES); do
-  FILE_32=$(echo "$LINE" | cut -d ':' -f 1)
-  FILE_64=$(echo "$LINE" | cut -d ':' -f 2)
-
-  MODULE=$(basename $FILE_32)
-  MODULE=${MODULE%.*}
-
-  echo "include \$(CLEAR_VARS)" >> $MK_ANDROID
-  echo "LOCAL_MULTILIB := both" >> $MK_ANDROID
-  echo "LOCAL_MODULE := $MODULE" >> $MK_ANDROID
-  echo "LOCAL_MODULE_OWNER := $VENDOR" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES_64 := proprietary/$FILE_64" >> $MK_ANDROID
-  echo "LOCAL_SRC_FILES_32 := proprietary/$FILE_32" >> $MK_ANDROID
-  echo "LOCAL_MODULE_TAGS := optional" >> $MK_ANDROID
-  echo "LOCAL_MODULE_SUFFIX := .so" >> $MK_ANDROID
-  echo "LOCAL_MODULE_CLASS := SHARED_LIBRARIES" >> $MK_ANDROID
-
-  if [[ "$FILE_32" =~ ^vendor\/.* ]]; then
-    echo "LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_PROPRIETARY_MODULE := true" >> $MK_ANDROID
-  else
-    echo "LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_SHARED_LIBRARIES)" >> $MK_ANDROID
-    echo "LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_SHARED_LIBRARIES)" >> $MK_ANDROID
-  fi
-
-  echo "include \$(BUILD_PREBUILT)" >> $MK_ANDROID
-  echo "" >> $MK_ANDROID
-done
-
-(cat << EOF) >> $MK_ANDROID
+cat << EOF >> "$ANDROIDMK"
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
-endif
 EOF
+
+# We are done!
+write_footers