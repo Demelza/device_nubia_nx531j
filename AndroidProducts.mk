LineageOS/android_device_nubia_nx531j

nx531j: clean up makefiles
Change-Id: Iae2cb06cb065f400ccee15cf89a5831403b30d08

AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_nx531j.mk
+    $(LOCAL_DIR)/cm.mk

Demelza/device_nubia_nx531j

AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_nx531j.mk
+    $(LOCAL_DIR)/mokee.mk