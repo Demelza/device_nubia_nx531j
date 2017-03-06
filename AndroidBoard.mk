48abc27c2b8276f6daed47dc4742fe7cc98ebb7d
XiNGRZ <chenxingyu92@gmail.com>
2017-03-06 18:09:37 +0800

    nx531j: Fixed failure of copying kernel headers, again
    Change-Id: I382d6f35bcffbc9b83278a2f0b015bcc9c0869c4

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-TARGET_PREBUILT_KERNEL_INCLUDE := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+TARGET_PREBUILT_KERNEL_INCLUDE := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 $(TARGET_PREBUILT_KERNEL_INCLUDE):
 	mkdir -p $@
-	cp -rf device/nubia/nx531j/kernel/include/* $@
+	cp -rf device/nubia/nx531j/kernel/include $@/
 
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP) $(TARGET_PREBUILT_KERNEL_INCLUDE)
 	$(transform-prebuilt-to-target)
